73aa3b0c6f0b814fdcd92781a3aaf2eab54e0cf7
CelsoGomesEM <celsogomes22@gmail.com>
2025-07-25 22:22:01 -0300

    Add migration of Lugar or Placements and execute migration for upgrade my DB with commands

---
+++ src/Passeio.Data/Mappings/LugarMapping.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Passeio.Negocio.Models;
+
+namespace Passeio.Data.Mappings
+{
+    public class LugarMapping : IEntityTypeConfiguration<Lugar>
+    {
+        public void Configure(EntityTypeBuilder<Lugar> builder)
+        {
+            builder.HasKey(l => l.Id);
+
+            builder.Property(l => l.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.Property(l => l.Localizacao)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.Property(l => l.UrlFoto)
+                .HasColumnType("varchar(250)");
+
+            builder.Property(l => l.Avaliacao)
+                .IsRequired();
+
+            builder.Property(l => l.CategoriaId)
+                .IsRequired();
+
+            // Relacionamento 1:1 com Categoria
+            builder.HasOne(l => l.Categoria)
+                .WithOne()
+                .HasForeignKey<Lugar>(l => l.CategoriaId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
