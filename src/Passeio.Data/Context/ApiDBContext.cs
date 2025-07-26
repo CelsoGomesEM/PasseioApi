593050b33d87e0552a81d82d9b12e7a727379cf6
CelsoGomesEM <celsogomes22@gmail.com>
2025-07-25 21:41:35 -0300

    Add new entity class and my context for EF Core.

---
+++ src/Passeio.Data/Context/ApiDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Passeio.Data.Context
+{
+    public class ApiDBContext : DbContext
+    {
+        public ApiDBContext(DbContextOptions<ApiDBContext> options) : base(options){}
+    }
+}
---
+++ src/Passeio.Negocio/Models/Lugar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passeio.Negocio.Models
+{
+    public class Lugar : Entity
+    {
+        public string Nome { get; set; }
+        public string Localizacao { get; set; }
+        public string UrlFoto { get; set; }
+        public int Avaliacao { get; set; }
+        public Categoria Categoria { get; set; }
+        public Guid CategoriaId { get; set; }
+    }
+}
