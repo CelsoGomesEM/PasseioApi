7ec013e0409973453f91172a8eb1ed8922a42fd9
CelsoGomesEM <celsogomes22@gmail.com>
2025-07-31 20:46:16 -0300

    Add documentation for swagger API

---
+++ src/Passeio.Api/Configuration/SwaggerConfig.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Passeio.Api.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                // Define o documento Swagger com uma única versão
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API - Passeios",
+                    Version = "v1",
+                    Description = "Api disponibilizada para nosso site de informações, avaliações sobre passeios e locais para viagem.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Celso Gomes",
+                        Email = "celsogomes22@gmail.com"
+                    }
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+
+            return services;
+        }
+
+    }
+}
--- src/Passeio.Api/Program.cs
+++ src/Passeio.Api/Program.cs
@@ -52,16 +52,16 @@ builder.Services.ResolveDependecies();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerConfig();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+});
 
 app.UseAuthentication();
 
